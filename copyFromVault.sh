9461334f955ca64285afd0059e27ed17c4d2929b
Andrew Stanton <mail@andrewstanton.tech>
2023-07-25 02:03:35 +0100

    Remove h1 and update links on copy

--- copyFromVault.sh
+++ copyFromVault.sh
@@ -6,3 +6,12 @@ rsync --verbose --progress --human-readable \
   /home/andrew/Dropbox/websites/cyberPontification/md
 
 mv ./md/cyberPontificationIndex.md ./md/index.md
+
+for file in md/*; do
+  echo "${file}"
+  # Remove title as that is generated from the metadata
+  sed -i '/^# .*$/d' "${file}"
+
+  # Convert Obsidian links with titles into URL links
+  sed -i -r 's/\[\[(.*?)\|(.*?)\]\]/[\2](\/\1.html)/g' "${file}"
+done
